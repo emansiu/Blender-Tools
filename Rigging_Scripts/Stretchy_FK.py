c5d518c69b061329a010af70f4cfd6929f8c6a03
EmanSiu <e@emanuelsiu.com>
2025-04-08 17:13:56 -0400

    rig mechanics working. creating icon and collections to polish off

--- Rigging_Scripts/Stretchy_FK.py
+++ Rigging_Scripts/Stretchy_FK.py
@@ -11,7 +11,18 @@ bl_info = {
 
 import bpy
 
-def rename_duplicated_bone(bone_to_rename):
+def DESELECT_ALL():
+    bpy.ops.armature.select_all(action='DESELECT')
+
+def select_bone(bone):
+    bone.select = True
+    bone.select_head = True
+    bone.select_tail = True
+
+def select_mesh_by_name(mesh_name):
+    bpy.data.objects[mesh_name].select_set(True)
+
+def rename_org_to_tweak(bone_to_rename):
     # ----RENAMING ----
     # replace any ORG's with MCH
     if "ORG" in bone_to_rename.name:
@@ -30,17 +41,73 @@ def rename_duplicated_bone(bone_to_rename):
         changed_bone_name = bone_to_rename.name[:index]
         bone_to_rename.name = bone_to_rename.name.replace(bone_to_rename.name,changed_bone_name)
 
+def rename_tweak_tip(bone_to_rename):
+    # ----RENAMING ----
+    # replace any ORG's with MCH
+    if "ORG" in bone_to_rename.name:
+        bone_to_rename.name = bone_to_rename.name.replace("ORG","MCH")
+
+    # add "tweak" to the appropriate location (not simply suffix - need to retain integrigy of .L or )
+    if ".L" in bone_to_rename.name:
+        index = bone_to_rename.name.find(".L")
+        changed_bone_name = bone_to_rename.name[:index] + "_tweak_tip"
+        bone_to_rename.name = bone_to_rename.name.replace(bone_to_rename.name,changed_bone_name)
+    else:
+        bone_to_rename.name = bone_to_rename.name.replace(bone_to_rename.name,bone_to_rename.name+"_tweak_tip")
+
+    if ".001" in bone_to_rename.name:
+        index = bone_to_rename.name.find(".001")
+        changed_bone_name = bone_to_rename.name[:index]
+        bone_to_rename.name = bone_to_rename.name.replace(bone_to_rename.name,changed_bone_name)
+
+def rename_org_to_fk(bone_to_rename):
+    # ----RENAMING ----
+    # replace any ORG's with FK
+    if "ORG" in bone_to_rename.name:
+        bone_to_rename.name = bone_to_rename.name.replace("ORG","FK")
+
+    if ".001" in bone_to_rename.name:
+        index = bone_to_rename.name.find(".001")
+        changed_bone_name = bone_to_rename.name[:index]
+        bone_to_rename.name = bone_to_rename.name.replace(bone_to_rename.name,changed_bone_name)
+
 # =================================== CLASS TO CREATE STRETCHY FK RIG ================================
 class VIEW3D_OT_StretchFK(bpy.types.Operator):
     """ Creates the stretchy FK system"""
     bl_idname = "object.stretchyfk"
     bl_label = "Stretchy FK Operator"
 
+    def create_icons(self):
+        #fk circle icon
+        fk_circle_icon = bpy.ops.curve.primitive_bezier_circle_add(radius=0.5)
+        tweak_icosphere_icon = bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=1,radius=0.5, enter_editmode=True)
+        bpy.ops.mesh.delete(type='ONLY_FACE')
+        # print(tweak_icosphere_icon)
+        # print(tweak_icosphere_icon.name)  
+        # DESELECT_ALL()
+        # select_mesh_by_name(tweak_icosphere_icon.name)
+        # bpy.ops.object.mode_set(mode='EDIT')
 
+    def create_collection_and_icons(self):
+        collection_name = "Armature_Icons"
+
+        if collection_name in bpy.data.collections:
+            print("this collection already exists")
+        else:
+            print("let's make this bad boy!!!")
+            new_collection = bpy.data.collections.new(collection_name)
+            bpy.context.scene.collection.children.link(new_collection)
 
 
     def execute(self, context):
-        print("============== NEW EXECUTION =================")
+
+        # self.create_collection_and_icons()
+        self.create_icons()
+
+
+        """
+
+        print("============== NEW STRETCHY EXECUTION =================")
         armature = bpy.context.object
         bones_down_the_chain = context.active_bone.children_recursive
 
@@ -48,37 +115,79 @@ class VIEW3D_OT_StretchFK(bpy.types.Operator):
         first_bone.use_connect = False
         original_bones = [context.active_bone.name]
 
+        # --- CREATE ONE BONE AT END; notice this is done AFTER variable "bones_down_the_chain" so as to not include it in loops concerning that chain.
+        DESELECT_ALL()
+        bones_down_the_chain[len(bones_down_the_chain)-1].select_tail = True
+        bpy.ops.armature.extrude_move(TRANSFORM_OT_translate={"value":(0.0,0.2,0.0)})
+        bpy.ops.armature.align()
 
+        tip_tweak_bone = bpy.context.active_bone
+        tip_tweak_bone.use_connect = False
+        tip_tweak_bone.color.palette = "THEME09"
+        rename_tweak_tip(tip_tweak_bone)
+
+
+    
+        DESELECT_ALL()
+        # -- select original chain and disconnect if connected to duplicate
+        select_bone(first_bone)
         for bone in bones_down_the_chain:
-            bone.use_connect = False
-            bpy.ops.armature.select_hierarchy(extend=True, direction="CHILD")
             original_bones.append(bone.name)
-            # --- if bones are connected, we disconnect them here, but keep them parented
-            
-
-        # with all bones selected, duplicate and resize 
-        # [!NOTICE!] --- when this operation is done it will select all the duplicated bones
-        bpy.ops.armature.duplicate()
-        bpy.ops.transform.resize(value= ( 0.5, 0.5, 0.5))
-
-        duplicated_bones = bpy.context.selected_bones
-        # Renaming duplicated bones, then parent originals to these new bones.
-        for index, bone in enumerate(duplicated_bones):
             bone.use_connect = False
-            print(f"index: '{index}' gives us bone: '{bone.name}'")
+            select_bone(bone)
 
-            rename_duplicated_bone(bone)
-            # --- ASSIGNING NEW PARENTS ----
-            print('-----  ORIGINAL BONES ------')
-            print(original_bones[index])
-            print('----- CURRENT DUPLICATED BONE ------')
-            print(bone.name)
+      
+        # [!NOTICE!] --- when this operation is done it will select all the duplicated bones
+        # --- create fk bones
+        bpy.ops.armature.duplicate()
+        fk_bones = bpy.context.selected_bones
+        bpy.ops.transform.resize(value= ( 0.75, 0.75, 0.75))
+        for bone in fk_bones:
+            rename_org_to_fk(bone)
+
+
+        #---- again, with all bones selected, duplicate and resize, asign to new variable
+        bpy.ops.armature.duplicate()
+        tweak_bones = bpy.context.selected_bones
+        bpy.ops.transform.resize(value= ( 0.75, 0.75, 0.75))
+
+        # ---- Renaming duplicated bones, then parent originals to these new bones. ----
+        for index, bone in enumerate(tweak_bones):
+            bone.use_connect = False
+            bone.color.palette = "THEME09"
+
+            rename_org_to_tweak(bone)
+            # --- PARENT ORIGINAL BONES TO NEW DUPLICATED (TWEAK) BONES ----
             armature.data.edit_bones[original_bones[index]].parent = armature.data.edit_bones[bone.name]
 
+            # --- PARENT NEW TWEAK BONES TO FK CONTROLLER BONES ----
+            armature.data.edit_bones[bone.name].parent = armature.data.edit_bones[fk_bones[index].name]
 
-            
+        # --- PARENT LONELY TIP TWEAK TO LAST FK CONTROLLER ----
+            armature.data.edit_bones[tip_tweak_bone.name].parent = armature.data.edit_bones[fk_bones[-1].name]
 
+
+        DESELECT_ALL()
+        # --- Assign constraints in pose bone mode --------
+        bpy.ops.object.mode_set(mode="POSE")
+        final_original_bone = len(original_bones)-1
+        for index, bone in enumerate(original_bones):
+
+            bone_constraint_owner = bpy.context.object.pose.bones.get(bone)
+            if index != final_original_bone:
+                bone_constraint_target = context.object.pose.bones.get(tweak_bones[index+1].name)
+            else:
+                bone_constraint_target = context.object.pose.bones.get(tip_tweak_bone.name)
+
+            print(f"constraining target: '{bone_constraint_target.name}' to owner: '{bone_constraint_owner.name}'")
+
+            constraint = bone_constraint_owner.constraints.new("STRETCH_TO")
+            constraint.target = armature
+            constraint.subtarget = bone_constraint_target.name
+                
         
+        
+        """
         return{'FINISHED'}
 
 
@@ -104,7 +213,6 @@ class VIEW3D_PT_CustomRigs(bpy.types.Panel):
                 return ("no object selected" , False )
 
             if selectedObject.mode == 'EDIT' and selectedObject.type == 'ARMATURE':
-                # print(f"Object '{selectedObject.name}' is in {selectedObject.mode} of type '{selectedObject.type}'.")
                 return True
             if len(bpy.context.active_bone.children_recursive) > 0:
                 return
@@ -138,3 +246,5 @@ if __name__ == "__main__":
     register()
     # You can run your operator or interact with the panel here...
     unregister()
+
+
