dd26e52b428d679d69a05e49f95582f430c389bf
emansiu <e@emanuelsiu.com>
2026-07-30 16:14:11 -0400

    testing clear naming structures and multi tool panel imports to main init panel

--- src/Emanate_Tools/__init__.py
+++ src/Emanate_Tools/__init__.py
@@ -1,20 +1,45 @@
 import bpy
+import sys
+import pkgutil
+import importlib
+
+from . import tools
 
 
 class EMANATE_STUDIOS_PT_panel(bpy.types.Panel):
-    bl_label = "Emanate Tools"
+    bl_label = "EMANATE STUDIOS Tools"
     bl_idname = "EMANATE_STUDIOS_PT_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = "Emanate"
+    bl_category = "EMANATE STUDIOS"
 
     def draw(self, context):
-        self.layout.label(text="Scaffold is working.")
+        pass
+
+
+_modules = []
+
+
+def _load_tool_modules():
+    found = []
+    for info in pkgutil.iter_modules(tools.__path__):
+        name = f"{tools.__name__}.{info.name}"
+        if name in sys.modules:
+            found.append(importlib.reload(sys.modules[name]))
+        else:
+            found.append(importlib.import_module(name))
+    return found
 
 
 def register():
     bpy.utils.register_class(EMANATE_STUDIOS_PT_panel)
+    _modules[:] = _load_tool_modules()
+    for mod in _modules:
+        mod.register()
 
 
 def unregister():
+    for mod in reversed(_modules):
+        mod.unregister()
+    _modules.clear()
     bpy.utils.unregister_class(EMANATE_STUDIOS_PT_panel)
---
+++ src/Emanate_Tools/tools/uv_checker.py
@@ -0,0 +1,47 @@
+import bpy
+from .. import naming_unity as naming
+
+NAMES = naming.register_tool(
+    "uv_checker",
+    label="UV Checker",
+    owner=__name__,
+    description="Report which UDIM tiles the selection uses",
+)
+
+
+class EMANATE_OT_uv_checker(bpy.types.Operator):
+    bl_idname = NAMES.operator_idname
+    bl_label = NAMES.label
+    bl_description = NAMES.description
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        self.report({'INFO'}, "Ran UV checker")
+        return {'FINISHED'}
+
+
+class EMANATE_PT_uv_checker(bpy.types.Panel):
+    bl_idname = NAMES.panel_idname
+    bl_label = NAMES.label
+    bl_parent_id = naming.ROOT_PANEL_IDNAME
+    bl_space_type = naming.SPACE_TYPE
+    bl_region_type = naming.REGION_TYPE
+    bl_options = {'DEFAULT_CLOSED'}
+    bl_order = NAMES.order
+
+    def draw(self, context):
+        self.layout.operator(NAMES.operator_idname)
+
+
+_classes = (EMANATE_OT_uv_checker, EMANATE_PT_uv_checker)
+
+
+def register():
+    naming.check_classes(_classes, NAMES)
+    for cls in _classes:
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+    for cls in reversed(_classes):
+        bpy.utils.unregister_class(cls)
